93c13593e8133691ce951369602a4b86fa363013
HutsonR <45318065+HutsonR@users.noreply.github.com>
2022-10-22 10:29:45 +0300

    update admins

--- src/components/admin/adminComponent/adminForm.vue
+++ src/components/admin/adminComponent/adminForm.vue
@@ -1,6 +1,22 @@
 <template>
 <div>
-Admin Form
+  <table class="admin_table">
+    <tr>
+      <th>ID</th>
+      <th>Наименование</th>
+      <th>....</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Ростсельмаш</td>
+      <td>20,00</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Линейка 20 см</td>
+      <td>30,00</td>
+    </tr>
+  </table>
 </div>
 </template>
 
