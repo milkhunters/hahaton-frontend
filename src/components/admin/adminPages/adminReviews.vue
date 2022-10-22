48f77d64ea578e23d14b560e150bd7013b7de3a7
Alim Dzhanibekov <71190177+alimdzhanibekov@users.noreply.github.com>
2022-10-22 21:03:40 +0300

    Update adminReviews.vue

--- src/components/admin/adminPages/adminReviews.vue
+++ src/components/admin/adminPages/adminReviews.vue
@@ -11,5 +11,29 @@ export default {
 </script>
 
 <style scoped>
-
+.admin_table {
+  margin-top: 20px;
+  width: 100%;
+}
+.admin_table tr {
+  border: 1px solid var(--border-light);
+}
+.admin_table th {
+  border-right: 2px solid var(--border-light);
+  border-bottom: 2px solid var(--border-light);
+  text-align: left;
+  padding: 4px;
+}
+.admin_table td {
+  border-right: 2px solid var(--border-light);
+  padding: 4px;
+  border-bottom: 2px solid var(--border-light);
+}
+.table_link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
 </style>
