41bd0843a58954c8fc8ba12c834c1bfa16429a2f
AlimAccount <alimdzhanibekov1@gmail.com>
2022-10-23 12:57:39 +0300

    fix alim 3

--- src/components/admin/ModalWindow/modalWindow.vue
+++ src/components/admin/ModalWindow/modalWindow.vue
@@ -49,6 +49,7 @@ export default {
   data() {
     return{
       // names: [{id:1, v:'ID', d: 'id'}, {id:2,v:'About', d:''}, {id:3,v:'Company_Url', d:''}, {id:4,v:'Cover', d:''}, {id:5,v:'Create_Time', d:''}, {id:6,v:'Description', d:''}, {id:7,v:'Exhibitor', d:''}, {id:8,v:'Import', d:''}, {id:9,v:'Inn', d:''}, {id:10,v:'Address', d:''}, {id:11,v:'Logo', d:''}, {id:12,v:'Manufacture', d:''}, {id:13,v:'PhoneNumber', d:''}, {id:14,v:'Title', d:''}, {id:15, v:'UpdateTime', d:''}],
+
       title: "",
       description: "",
       about: "",
@@ -76,7 +77,7 @@ export default {
       this.$emit('update:show', false)
     },
     async updateForm() {
-      const response = await axios.post('https://dev-hack.milkhunters.ru/api/v1/admin/company/update',{
+      const response = await axios.post('https://dev-hack.milkhunters.ru/api/v1/admin/company/update?company_id=' + this.content.id,{
         "title": this.title,
         "description": this.description,
         "about": this.about,
